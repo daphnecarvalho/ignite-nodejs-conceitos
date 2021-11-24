11970038f2fc905114244df3128ad74071479d88
Daphne Carvalho <dah.feh@hotmail.com>
2021-11-23 22:01:07 -0300

    implementing index.js and update README.md

--- README.md
+++ README.md
@@ -1,18 +1,33 @@
 # TODOs
 
 Aplicação para gerenciar tarefas.
+## :computer: Projeto
 
-## :orange_book: Requisitos
+### :orange_book: Requisitos
 
 - Criação de um usuário com `name` e `username`
 - CRUD de TODOs
-  1. Criar um novo *todo*;
-  2. Listar todos os *todos*;
+  1. Listar todos os *todos*;
+  2. Criar um novo *todo*;
   3. Alterar o `title` e `deadline` de um *todo* existente;
   4. Marcar um *todo* como feito;
   5. Excluir um *todo*;
 
-## :computer: Projeto
+### :straight_ruler: Regras de negócio
+
+#### :ok_woman: Teste de Usuários
+- [x] Should be able to create a new user
+- [x] Should not be able to create a new user when username already exists
+
+#### :page_facing_up: Teste de _todos_
+- [x] Should be able to list all user's todos
+- [x] Should be able to create a new todo
+- [x] Should be able to update a todo
+- [x] Should not be able to update a non existing todo
+- [x] Should be able to mark a todo as done
+- [x] Should not be able to mark a non existing todo as done
+- [x] Should be able to delete a todo
+- [x] Should not be able to delete a non existing todo
 
 ### :memo: Executando o Projeto
 
--- src/index.js
+++ src/index.js
@@ -1,41 +1,118 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => username === user.username);
+
+  if(!user) {
+    return response.status(404).json({ error: "User not found!"});
+  }
+
+  request.user = user;
+
+  return next();
+}
+
+function getTodo(response, user, id) {
+  const todo = user.todos.find((todo) => todo.id === id);
+
+  if(!todo) {
+    return response.status(404).json({ error: "User TODO not found!"});
+  }
+
+  return todo;
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+
+  const checksExistsUserAccount = users.some(
+    (user) => username === user.username
+  );
+
+  if(checksExistsUserAccount) {
+    return response.status(400).json({ error: "Username already exists!"});
+  }
+
+  const user = {
+    id: uuidv4(),
+    name: name,
+    username: username,
+    todos: []
+  }
+
+  users.push(user);
+
+  return response.status(201).json(user);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+
+  return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { title, deadline } = request.body;
+
+  const todo = {
+    id: uuidv4(),
+    title: title,
+    done: false, 
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(todo);
+
+  return response.status(201).json(todo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const { title, deadline } = request.body;
+  
+  const todo = getTodo(response, user, id);
+
+  todo.title = title;
+  todo.deadline = new Date(deadline);
+
+  return response.json(todo);
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = getTodo(response, user, id);
+
+  todo.done = true;
+
+  return response.json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+
+  const todo = getTodo(response, user, id);
+
+  user.todos.splice(todo, 1);
+
+  return response.status(204).send();
 });
 
 module.exports = app;
